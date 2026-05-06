be38e20fb6baefeb773b408284f78f336f2ea076
Morais Garcia Gimo <moraisgarciagimo13@gmail.com>
2026-05-06 19:50:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLÁMUNDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OLA MUNDO");
+            Console.ReadLine();
+            // console.writeLine("ola mundo");
+            int x = 12;
+            int y = 45;
+            int soma = x + y;
+            // string consolta;
+            // double z;
+            // bool verdadeiro;
+
+            Console.WriteLine("a soma dos numero e" + soma);
+            Console.ReadLine();
+        }
+    }
+}
